29077e576970f36d965a03a6de2a9ac77c0ca6d4
Magda Środa <magdasroda@MacBook-Air.local>
2026-01-20 19:54:33 +0100

    Fixing an issue with login

--- tests/lyro.test.ts
+++ tests/lyro.test.ts
@@ -1,8 +1,5 @@
 import { expect, test } from "@playwright/test";
 import dotenv from 'dotenv';
-import { LoginPage } from "../pages/LoginPage.ts";
-import { DataSourcesPage } from "../pages/DataSourcesPage.ts";
-import { SimulatePage } from "../pages/SimulatePage.ts";
 import { randomString } from "../utils/functions.js";
 
 dotenv.config();
@@ -19,56 +16,45 @@ test.describe("Lyro AI tests", () => {
     });
 
     await test.step("Test from data sources section", async () => {
-      const dataSourcesPage = new DataSourcesPage(page);
-      const appContentHeader = dataSourcesPage.appContentHeader;
-      const dataSourcesSection = dataSourcesPage.dataSourcesSection;
-      const testLyroButton = dataSourcesPage.testLyroButton;
-      const testLyroInputMessage = dataSourcesPage.testLyroInputMessage;
       const randomMessage = randomString(8);
-
-      await page.goto(
-        `https://${process.env.DOMAIN}/panel/lyro-ai/data-sources/added`
-      );
+      const appContentHeader = page.locator("#app-content-header");
+      const dataSourcesSection = page.getByRole("heading", { name: "Data sources" });
+      const testLyroButton = page.getByRole("button", { name: "Test Lyro" });
+      const testLyroChatFrame = page.frameLocator('iframe[title="Tidio Chat code"]');
+      
+      await page.goto(`https://${process.env.DOMAIN}/panel/lyro-ai/data-sources/added`);
       await expect(appContentHeader.getByText("Lyro AI Agent")).toBeInViewport();
       await expect(dataSourcesSection).toBeInViewport();
       await testLyroButton.click();
 
-      await expect(
-        page.getByText("Hello, how can I help you?", { exact: false })
-      ).toBeInViewport();
-      await expect(testLyroInputMessage).toBeVisible();
-      await testLyroInputMessage.click();
-      await testLyroInputMessage.fill("What Magda did on 17th of January 2026?");
-      await testLyroInputMessage.press("Enter");
-      await expect(page.getByText("On January 17, 2026, Magda Środa made the initial commit")
-    ).toBeInViewport();
+      const testLyroMessageInput = testLyroChatFrame.getByTestId('newMessageTextarea');
+      await testLyroMessageInput.click();
+      await testLyroMessageInput.fill("What Magda did on 17th of January 2026?");
+      await testLyroMessageInput.press("Enter");
+      await expect(page.getByText("On January 17, 2026, Magda Środa made the initial commit")).toBeInViewport();
 
-      await testLyroInputMessage.fill(randomMessage);
-      await testLyroInputMessage.press("Enter");
-      await expect(
-        page.getByText("I'm not sure what you're trying to ask. Could you please share your question so I can assist you better? ")
-      ).toBeInViewport();
+      await testLyroMessageInput.fill(randomMessage);
+      await testLyroMessageInput.press("Enter");
+      await expect(page.getByText("I'm not sure what you're trying to ask. Could you please share your question so I can assist you better? ")).toBeInViewport();
     });
 
     await test.step("Simulate visitor and test from widget side", async () => {
-      const simulatePage = new SimulatePage(page);
-      const inboxSection = simulatePage.inboxSection;
-      const simulateConversationButton = simulatePage.simulateConversationButton;
-      const simulateVisitorInputMessage = simulatePage.simulateVisitorInputMessage;
-      const chatWithUsButton = simulatePage.chatWithUsButton;
       const simulateVisitorRandomMessage = randomString(7);
-
+      const inboxSection = page.getByRole("heading", { name: "Inbox" });
+      const simulateConversationButton = page.getByRole("button", { name: "Simulate a conversation" });
+      const chatWithUsButton = page.locator('button:has-text("Chat with us")');
+      const chat = page.frameLocator('iframe[title="Tidio Chat code"]');
+      
       await page.locator('[data-test-id="inbox-section-button"]').click();
       await expect(inboxSection).toBeInViewport();
       
-      const [simulateVisitorPage] = await Promise.all([
-        page.waitForEvent("popup"),simulateConversationButton.click(),
-      ]);
+      const [simulateVisitorPage] = await Promise.all([page.waitForEvent("popup"),simulateConversationButton.click(),]);
       
       await simulateVisitorPage.waitForLoadState();
       await expect(simulateVisitorPage)
       .toHaveURL("https://www.tidio.com/panel/simulateVisitor?projectPublicKey=b7exsgtovbxc3jyuceg2nzn8jrq4rkcz");
       
+      const simulateVisitorInputMessage = chat.getByTestId('newMessageTextarea');
       if (!(await simulateVisitorInputMessage.isVisible())) {
         await chatWithUsButton.click();
         await expect(simulateVisitorInputMessage).toBeVisible({ timeout: 5000 });
